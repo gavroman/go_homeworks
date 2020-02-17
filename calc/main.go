292bbc99cfd8e2c862bab50602db222606d18da0
gavroman <gavroman1998@gmail.com>
2020-02-18 02:00:03 +0300

    Calc done

--- calc/main.go
+++ calc/main.go
@@ -1,71 +1,117 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	Stack "github.com/golang-collections/collections/stack"
+    "errors"
+    "flag"
+    "fmt"
+    Stack "github.com/golang-collections/collections/stack"
+    "strconv"
 )
 
 func isDigit(char int32) bool {
-	return char >= '0' && char <= '9'
+    return char >= '0' && char <= '9'
 
 }
 
+func reversePolishNotation(expr string) ([]string, error) {
+    resultArray := make([]string, 0, len(expr))
+    stack := Stack.New()
+    numbers := ""
+    for _, char := range expr {
+        if isDigit(char) {
+            numbers += string(char)
+        } else {
+            if numbers != "" {
+                resultArray = append(resultArray, numbers)
+                numbers = ""
+            }
+            switch char {
+            case '+', '-':
+                for stack.Len() != 0 && stack.Peek() != "(" {
+                    got := fmt.Sprintf("%v", stack.Pop())
+                    resultArray = append(resultArray, got)
+                }
+                stack.Push(string(char))
+
+            case '*', '/':
+                if stack.Peek() == "*" || stack.Peek() == "/" {
+                    for stack.Len() != 0 && stack.Peek() != "+" && stack.Peek() != "-" {
+                        got := fmt.Sprintf("%v", stack.Pop())
+                        resultArray = append(resultArray, got)
+                    }
+                }
+                stack.Push(string(char))
+
+            case '(':
+                stack.Push(string(char))
+
+            case ')':
+                for stack.Len() != 0 && stack.Peek() != "(" {
+                    got := fmt.Sprintf("%v", stack.Pop())
+                    resultArray = append(resultArray, got)
+                }
+                stack.Pop()
+            default:
+                return []string{}, errors.New("wrong character in expression")
+            }
+        }
+    }
+    if numbers != "" {
+        resultArray = append(resultArray, numbers)
+    }
+    for stack.Len() != 0 {
+        got := fmt.Sprintf("%v", stack.Pop())
+        resultArray = append(resultArray, got)
+    }
+    return resultArray, nil
+}
+
+func calcFromPolishNotation(exprArray []string) (float64, error) {
+    stack := Stack.New()
+    for i := range exprArray {
+        number, err := strconv.Atoi(exprArray[i])
+        if err == nil {
+            stack.Push(number)
+        } else {
+            right, err := strconv.ParseFloat(fmt.Sprintf("%v", stack.Pop()), 64)
+            left, err := strconv.ParseFloat(fmt.Sprintf("%v", stack.Pop()), 64)
+            if err != nil {
+                return 0, err
+            }
+            switch exprArray[i] {
+            case "+":
+                stack.Push(left + right)
+            case "-":
+                stack.Push(left - right)
+            case "*":
+                stack.Push(left * right)
+            case "/":
+                stack.Push(left / right)
+            default:
+                return 0, errors.New("wrong operation character")
+            }
+        }
+    }
+    result, err := strconv.ParseFloat(fmt.Sprintf("%v", stack.Pop()), 64)
+    return result, err
+}
+
 func main() {
-	flag.Parse()
-	args := flag.Args()
+    flag.Parse()
+    args := flag.Args()
 
-	expressionString := ""
-	for i := range args {
-		expressionString += args[i]
-	}
+    expressionString := ""
+    for i := range args {
+        expressionString += args[i]
+    }
 
-	outputArray := make([]string, 0, len(expressionString))
-	operationStack := Stack.New()
-	numberString := ""
-	for _, char := range expressionString {
-		if isDigit(char) {
-			numberString += string(char)
-		} else {
-			if numberString != "" {
-				outputArray = append(outputArray, numberString)
-				numberString = ""
-			}
-			switch char {
-			case '+', '-':
-				for operationStack.Len() != 0 {
-					if operationStack.Peek() == "(" {
-						break
-					}
-					got := fmt.Sprintf("%v", operationStack.Pop())
-					outputArray = append(outputArray, got)
-				}
-				operationStack.Push(string(char))
-
-			case '*', '/', '(':
-				operationStack.Push(string(char))
-
-			case ')':
-				for operationStack.Len() != 0 {
-					if operationStack.Peek() == "(" {
-						break
-					}
-					got := fmt.Sprintf("%v", operationStack.Pop())
-					outputArray = append(outputArray, got)
-				}
-				operationStack.Pop()
-			}
-
-		}
-	}
-	outputArray = append(outputArray, numberString)
-	for operationStack.Len() != 0 {
-		got := fmt.Sprintf("%v", operationStack.Pop())
-		outputArray = append(outputArray, got)
-	}
-
-	// fmt.Println(outputArray)
-	for i := range outputArray {
-		fmt.Print(outputArray[i]," , ")
-	}
+    outputArray, err := reversePolishNotation(expressionString)
+    if err != nil {
+        return
+    }
+    fmt.Println(outputArray)
+    result, err := calcFromPolishNotation(outputArray)
+    if err == nil {
+        fmt.Println(result)
+    }
 }
--- sort/main.go
+++ sort/main.go
@@ -1,132 +1,132 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"os"
-	"sort"
-	conv "strconv"
-	strs "strings"
+    "bufio"
+    "flag"
+    "fmt"
+    "os"
+    "sort"
+    conv "strconv"
+    strs "strings"
 )
 
 func readStringsFromFile(filename string) []string {
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-		return []string{}
-	}
+    file, err := os.Open(filename)
+    if err != nil {
+        fmt.Println(err)
+        return []string{}
+    }
 
-	reader := bufio.NewReader(file)
+    reader := bufio.NewReader(file)
 
-	var str string
-	result := make([]string, 0, 8)
-	for err == nil {
-		str, err = reader.ReadString('\n')
-		str = strs.TrimRight(str, "\n")
-		result = append(result, str)
-	}
+    var str string
+    result := make([]string, 0, 8)
+    for err == nil {
+        str, err = reader.ReadString('\n')
+        str = strs.TrimRight(str, "\n")
+        result = append(result, str)
+    }
 
-	if result[len(result)-1] == "" {
-		result = result[:len(result)-1]
-	}
+    if result[len(result)-1] == "" {
+        result = result[:len(result)-1]
+    }
 
-	return result
+    return result
 }
 
 func writeStringsToFile(stringsToWrite *[]string, filename string) error {
-	if stringsToWrite == nil {
-		return nil
-	}
+    if stringsToWrite == nil {
+        return nil
+    }
 
-	outputFile, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer outputFile.Close()
-	writer := bufio.NewWriter(outputFile)
-	for i := range *stringsToWrite {
-		(*stringsToWrite)[i] += "\n"
-		_, err := writer.WriteString((*stringsToWrite)[i])
-		if err != nil {
-			return err
-		}
-	}
-	writer.Flush()
-	return nil
+    outputFile, err := os.Create(filename)
+    if err != nil {
+        return err
+    }
+    defer outputFile.Close()
+    writer := bufio.NewWriter(outputFile)
+    for i := range *stringsToWrite {
+        (*stringsToWrite)[i] += "\n"
+        _, err := writer.WriteString((*stringsToWrite)[i])
+        if err != nil {
+            return err
+        }
+    }
+    writer.Flush()
+    return nil
 }
 
 func removeDuplicates(slice []string, equal func(l, r string) bool) []string {
-	n := len(slice)
-	if n <= 1 {
-		return slice
-	}
-	j := 1
-	for i := 1; i != n; i++ {
-		if !equal(slice[i], slice[i-1]) {
-			slice[j] = slice[i]
-			j++
-		}
-	}
-	return slice[:j]
+    n := len(slice)
+    if n <= 1 {
+        return slice
+    }
+    j := 1
+    for i := 1; i != n; i++ {
+        if !equal(slice[i], slice[i-1]) {
+            slice[j] = slice[i]
+            j++
+        }
+    }
+    return slice[:j]
 }
 
 func reverseSlice(slice *[]string) {
-	for i := len(*slice)/2 - 1; i != -1; i-- {
-		j := len(*slice) - 1 - i
-		(*slice)[i], (*slice)[j] = (*slice)[j], (*slice)[i]
-	}
+    for i := len(*slice)/2 - 1; i != -1; i-- {
+        j := len(*slice) - 1 - i
+        (*slice)[i], (*slice)[j] = (*slice)[j], (*slice)[i]
+    }
 }
 
 func getWordFromString(s *string, position int) string {
-	result := ""
-	words := strs.Fields(*s)
-	if position < len(words) {
-		result = words[position]
-	}
-	return result
+    result := ""
+    words := strs.Fields(*s)
+    if position < len(words) {
+        result = words[position]
+    }
+    return result
 }
 
 func sortStrings(stringSlice *[]string, flags map[byte]*bool, kFlagVal int) {
-	less := func(i, j int) bool {
-		lString, rString := (*stringSlice)[i], (*stringSlice)[j]
-		if kFlagVal != -1 {
-			lString = getWordFromString(&lString, kFlagVal)
-			rString = getWordFromString(&rString, kFlagVal)
-		}
-		if *flags['n'] {
-			lNumber, _ := conv.Atoi(lString)
-			rNumber, _ := conv.Atoi(rString)
-			return lNumber < rNumber
-		}
-		if *flags['f'] {
-			lString, rString = strs.ToLower(lString), strs.ToLower(rString)
-		}
-		return lString < rString
-	}
-	sort.Slice(*stringSlice, less)
+    less := func(i, j int) bool {
+        lString, rString := (*stringSlice)[i], (*stringSlice)[j]
+        if kFlagVal != -1 {
+            lString = getWordFromString(&lString, kFlagVal)
+            rString = getWordFromString(&rString, kFlagVal)
+        }
+        if *flags['n'] {
+            lNumber, _ := conv.Atoi(lString)
+            rNumber, _ := conv.Atoi(rString)
+            return lNumber < rNumber
+        }
+        if *flags['f'] {
+            lString, rString = strs.ToLower(lString), strs.ToLower(rString)
+        }
+        return lString < rString
+    }
+    sort.Slice(*stringSlice, less)
 
-	if *flags['u'] {
-		equal := func(l, r string) bool {
-			if kFlagVal != -1 {
-				l = getWordFromString(&l, kFlagVal)
-				r = getWordFromString(&r, kFlagVal)
-			}
-			if *flags['n'] {
-				lNumber, _ := conv.Atoi(l)
-				rNumber, _ := conv.Atoi(r)
-				return lNumber == rNumber
-			}
-			if *flags['f'] {
-				l, r = strs.ToLower(l), strs.ToLower(r)
-			}
-			return l == r
-		}
-		*stringSlice = removeDuplicates(*stringSlice, equal)
-	}
-	if *flags['r'] {
-		reverseSlice(stringSlice)
-	}
+    if *flags['u'] {
+        equal := func(l, r string) bool {
+            if kFlagVal != -1 {
+                l = getWordFromString(&l, kFlagVal)
+                r = getWordFromString(&r, kFlagVal)
+            }
+            if *flags['n'] {
+                lNumber, _ := conv.Atoi(l)
+                rNumber, _ := conv.Atoi(r)
+                return lNumber == rNumber
+            }
+            if *flags['f'] {
+                l, r = strs.ToLower(l), strs.ToLower(r)
+            }
+            return l == r
+        }
+        *stringSlice = removeDuplicates(*stringSlice, equal)
+    }
+    if *flags['r'] {
+        reverseSlice(stringSlice)
+    }
 }
 
 func main() {
